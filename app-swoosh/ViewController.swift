eeb6ebd15635c5375e2e90a84160c290cb23cad7
Alejandro Pardo Rodriguez <alejandropro@ext.inditex.com>
2020-02-19 15:55:01 +0100

    Adds segue between first and second screen and back button

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -16,4 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
