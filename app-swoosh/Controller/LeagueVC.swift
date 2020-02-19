db878bfbd78cad38f9ffcd2cba69d9de80e04058
Alejandro Pardo Rodriguez <alejandropro@ext.inditex.com>
2020-02-19 16:26:14 +0100

    Adds controllers for all views

---
+++ app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Alejandro Pardo on 19/02/2020.
+//  Copyright © 2020 Alejandro Pardo. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Alejandro Pardo on 19/02/2020.
+//  Copyright © 2020 Alejandro Pardo. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
